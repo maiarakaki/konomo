3bd1736fdedbc0a6da38125a38dfb9e454b73989
WQ248VG <maite.arakaki@gmail.com>
2021-06-14 11:24:36 -0300

    20210614Win

--- src/ar/com/konomo/Main.java
+++ src/ar/com/konomo/Main.java
@@ -1,163 +1,16 @@
 package ar.com.konomo;
 
-import ar.com.konomo.display.Display;
-import ar.com.konomo.entity.*;
-import ar.com.konomo.enums.Action;
-import ar.com.konomo.managers.GM;
-import ar.com.konomo.operators.AttackLogger;
-import ar.com.konomo.operators.EventMessageLog;
-import ar.com.konomo.validators.WinValidator;
-//import com.google.gson.Gson;
+import ar.com.konomo.server.logic.Game;
 
-import java.util.*;
 
 public class Main {
-    private String ip;
-    private String port;
     public static final int NINJAS = 3;
     public static final int BOARD_SIZE = 5;
 
     public static void main(String[] args) {
-        GM gameManager = new GM();
-        gameManager.createGame();
-        Player player1 = gameManager.getPlayer1();
-        player1.setName("Jugador 1");
-        Player player2 = gameManager.getPlayer2();
-        AttackLogger attackLogger = new AttackLogger();
-        WinValidator winValidator = new WinValidator();
+        Game game = new Game();
 
-        player2.setName("Jugador 2");
-        Display display = new Display();
-        //display.titleScreen();
-        //String userOption = display.showOptions();
-        List<Coordinate> coordinates = new ArrayList<>();
-        //coordinates= (display.playerSettings(player1));
-        //SETEO LOS NIJS DEL P1 EN A1 (10:0) ; B1 (11:0) y D1 (13:0)
-        { // TODO boletear esto cuando termine las pruebas con las coordenadas
-            Coordinate coord1 = new Coordinate(10, 0);
-            coordinates.add(coord1);
-            Coordinate coord2 = new Coordinate(11, 0);
-            Coordinate coord3 = new Coordinate(13, 0);
-            coordinates.add(coord2);
-            coordinates.add(coord3);
-        }
-//VALIDO Y MUESTRO COMO QUEDARON LOS TABLEROS DEL P1
-        boolean allGood = gameManager.validate(coordinates, player1);
-        if (allGood) {
-            display.retrieveBoard(player1);
-        } else {
-            while (!allGood) {
-                OpError errors = gameManager.getErrors();
-                coordinates = display.ammendCoordinates(coordinates, errors);
-                allGood = gameManager.validate(coordinates, player1);
-            }
-        }
-//SETEO LOS NIJAS DEL P2 EN B3 (11:2), D5 (13:4) Y E1 (14:0)
-        { // TODO boletear esto cuando termine las pruebas con las coordenadas
-            Coordinate coord1 = new Coordinate(11, 2);
-            coordinates.set(0, coord1);
-            Coordinate coord2 = new Coordinate(13, 4);
-            coordinates.set(1, coord2);
-            Coordinate coord3 = new Coordinate(14, 0);
-            coordinates.set(2, coord3);
-        }
-        //VALIDO, ETC
-        allGood = gameManager.validate(coordinates, player2);
-        if (allGood) {
-            display.retrieveBoard(player2);
-
-        } else {
-            while (!allGood) {
-                OpError errors = gameManager.getErrors();
-                coordinates = display.ammendCoordinates(coordinates, errors);
-                allGood = gameManager.validate(coordinates, player2);
-            }
-        }
-        //  display.retrieveBoard(player2);
-
-        //ATACO CON EL P1 A TODOS LOS NINJAS DEL P2
-
-
-        Map<Integer, Intention> playerIntentions = new HashMap<>();
-        Player playerInTurn= player1;
-        Player adversary = player2;
-
-        boolean gameOver = winValidator.winConditionsMet(player1, player2);
-
-        while (!gameOver) {
-            //al inicio del turno me recibo los ataques de mi oponente...
-
-            if (!gameManager.getAttackLogs().getAttackLog().isEmpty()) {
-                gameManager.updateBoards(playerInTurn, gameManager.getAttackLogs().getAttackLog());
-                gameManager.getEventLog().show();
-                gameOver = winValidator.winConditionsMet(player1, player2);
-            }
-
-            if (!gameOver) {
-                display.retrieveBoard(playerInTurn);
-                playerIntentions = display.gamePlay(playerInTurn);
-                allGood = gameManager.validate(playerIntentions, playerInTurn);
-
-                while (!allGood) {
-                    OpError errors = gameManager.getErrors();
-                    playerIntentions = display.ammendIntentions(playerIntentions, errors, playerInTurn);
-                    allGood = gameManager.validate(playerIntentions, playerInTurn);
-                }
-                gameManager.updateBoards(playerInTurn, playerIntentions, adversary.getLocalBoard());
-                gameManager.getEventLog().show();
-                display.retrieveBoard(playerInTurn);
-
-
-                if (playerInTurn == player1) {
-                    playerInTurn = player2;
-                    adversary = player1;
-                } else {
-                    playerInTurn = player1;
-                    adversary = player2;}
-                gameOver = winValidator.winConditionsMet(player1, player2);
-
-                System.out.println("==============FIN DE TURNO=============");
-                }
-            }
-
-
-        System.out.println("GAME OVERRRRR");
-        System.out.println(winValidator.getWinner());
-
-        //al comienzo del turno, reviso el attackLog OJO, AL FINAL DE CADA TURNO DEBE PISARSE CON EL ATAQUE DEL JUGADOR Q ESTA JUGANDO
-
-        //WinValidator winValidator = new WinValidator();
-
-/*
-        while (!winValidator.winConditionsMet(player1, player2)) {
-        List<Intention> opponentAttacks = new ArrayList<>();
-
-
-
-            //setear turno
-            //get player actions
-            //validar
-            //update board
-            //check win
-        }
-*/
-
-
-}
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public void setPort(String port) {
-        this.port = port;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public String getPort() {
-        return port;
+        game.start();
+        game.play();
     }
 }
--- src/ar/com/konomo/server/logic/Game.java
+++ src/ar/com/konomo/server/logic/Game.java
@@ -1,11 +1,118 @@
 package ar.com.konomo.server.logic;
 
+import ar.com.konomo.display.Display;
+import ar.com.konomo.entity.*;
+import ar.com.konomo.managers.GM;
 import ar.com.konomo.server.Server;
+import ar.com.konomo.validators.WinValidator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Game {
-    private Server server;
 
-    public Game(Server server) {
-        this.server = server;
+    private Server server;
+    private GM gameManager;
+    private Display display;
+    private Player player1;
+    private Player player2;
+    private WinValidator winValidator;
+    private List<Coordinate> coordinates;
+    private Player playerInTurn;
+    private Player adversary;
+
+    public Game (){
+        server = new Server();
+        gameManager = new GM();
+        display = new Display();
+        coordinates = new ArrayList<>();
+        winValidator = new WinValidator();
+
     }
+
+    public void start(){
+        gameManager.createGame();
+        player1 = gameManager.getPlayer1();
+        player2 = gameManager.getPlayer2();
+        try {
+            server.start();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage() + " failed to start Server");
+        }
+
+        display.titleScreen();
+        //String userOption = display.showOptions();
+
+        playerInTurn = getPlayer(player1);
+        adversary = getPlayer(player2);
+    }
+
+
+    private Player getPlayer(Player player){
+        coordinates= (display.playerSettings(player));
+        boolean allGood = gameManager.validate(coordinates, player);
+        if (allGood) {
+            display.retrieveBoard(player);
+        } else {
+            while (!allGood) {
+                OpError errors = gameManager.getErrors();
+                coordinates = display.ammendCoordinates(coordinates, errors);
+                allGood = gameManager.validate(coordinates, player);
+            }
+        }
+        return player;
+    }
+
+    public void play(){
+        boolean gameOver = winValidator.winConditionsMet(player1, player2);
+        boolean allGood;
+
+        while (!gameOver) {
+            //al inicio del turno me recibo los ataques de mi oponente...
+            Map<Integer, Intention> playerIntentions = new HashMap<>();
+
+
+            if (!gameManager.getAttackLogs().getAttackLog().isEmpty()) {
+                gameManager.updateBoards(playerInTurn, gameManager.getAttackLogs().getAttackLog());
+                gameManager.getEventLog().show();
+                gameOver = winValidator.winConditionsMet(player1, player2);
+            }
+
+            if (!gameOver) {
+                display.retrieveBoard(playerInTurn);
+                playerIntentions = display.gamePlay(playerInTurn);
+                allGood = gameManager.validate(playerIntentions, playerInTurn);
+
+                while (!allGood) {
+                    OpError errors = gameManager.getErrors();
+                    playerIntentions = display.ammendIntentions(playerIntentions, errors, playerInTurn);
+                    allGood = gameManager.validate(playerIntentions, playerInTurn);
+                }
+                gameManager.updateBoards(playerInTurn, playerIntentions, adversary.getLocalBoard());
+                gameManager.getEventLog().show();
+                display.retrieveBoard(playerInTurn);
+
+
+                if (playerInTurn == player1) {
+                    playerInTurn = player2;
+                    adversary = player1;
+                } else {
+                    playerInTurn = player1;
+                    adversary = player2;}
+                gameOver = winValidator.winConditionsMet(player1, player2);
+
+                System.out.println("==============FIN DE TURNO=============");
+            }
+        }
+
+
+        System.out.println("GAME OVERRRRR");
+        System.out.println("Ganador: " + winValidator.getWinner());
+    }
+
+
+
+
 }
