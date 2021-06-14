6a924019bb7634711f6e9bc114a3d3bde3a11dee
WQ248VG <maite.arakaki@gmail.com>
2021-06-14 09:30:02 -0300

    20210612Win

--- src/ar/com/konomo/validators/IntentionViabilityValidator.java
+++ src/ar/com/konomo/validators/IntentionViabilityValidator.java
@@ -19,19 +19,35 @@ public class IntentionViabilityValidator {
     }
 
     public boolean isViable(List<Intention> intentionsList){
+        boolean isViable= false;
+        int intentionNum= intentionsList.size();
         final int INT1 = 0;
         final int INT2 = 1;
         final int INT3 = 2;
 
-        oneAndTwo = compare(intentionsList.get(INT1),intentionsList.get(INT2));
-        oneAndThree = compare(intentionsList.get(INT1),intentionsList.get(INT3));
-        twoAndThree = compare(intentionsList.get(INT2), intentionsList.get(INT3));
+        switch(intentionNum) {
+            case 3:
+                oneAndTwo = compare(intentionsList.get(INT1),intentionsList.get(INT2));
+                oneAndThree = compare(intentionsList.get(INT1),intentionsList.get(INT3));
+                twoAndThree = compare(intentionsList.get(INT2), intentionsList.get(INT3));
+                isViable = ! (oneAndTwo || oneAndThree || twoAndThree);
+                break;
+            case 2:
+                oneAndTwo = compare(intentionsList.get(INT1),intentionsList.get(INT2));
+                isViable = !oneAndTwo;
+                break;
+            case 1:
+                isViable = true;
+                break;
+            case 0:
+                isViable = false;
+        }
 
-        if (oneAndTwo || oneAndThree || twoAndThree) {
+        if(!isViable){
             errors.add(NON_VIABLE_INT +": " + NON_VIABLE_INT_MSG);
         }
 
-        return !(oneAndTwo || oneAndThree || twoAndThree);
+        return isViable;
     }
 
     private boolean compare(Intention intentionA, Intention intentionB) {
